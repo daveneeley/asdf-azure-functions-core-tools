a251b5badabb7397a97e90eca1fb82f8c3668e6b
Dave Neeley <dave.neeley@gmail.com>
2022-06-04 01:43:52 -0600

    recurse

--- lib/utils.bash
+++ lib/utils.bash
@@ -55,7 +55,7 @@ install_version() {
 
   (
     mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    cp -R "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
